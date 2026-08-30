164798fc359fabec3ba654a6ebbaaca4bc9d5260
nx3cky <mykolahe1ko@gmail.com>
2026-08-30 14:52:15 +0300

    changed Index view and created SprintTasks (task 1-2)

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace vs.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult SprintTasks()
+        {
+            return View();
+        }
+    }
+}
