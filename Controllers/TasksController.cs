cdce2408b8fd59bb32b6fbd759ff72e48ed6fb0c
Misha Lukianiuk <misha01062008@gmail.com>
2026-08-30 16:37:56 +0300

    add Get/post actions for Shopping cart, add ShoppingCart view

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -45,5 +45,35 @@ namespace vs.Controllers
 
             return View(shoppingList);
         }
+        [HttpGet]
+        public IActionResult ShoppingCart()
+        {
+            List<string> markets = new List<string>
+            {
+                "WellMart",
+                "Silpo",
+                "ATB",
+                "Furshet",
+                "Metro"
+            };
+            Dictionary<string, int> shoppingList = new Dictionary<string, int>
+            {
+                 { "Milk", 2 },
+                 { "Bread", 2 },
+                 { "Cake", 1 },
+                 { "Ice Cream", 5 },
+                 { "Cola", 10 }
+            };
+            ViewBag.SuperMarkets = markets;
+            ViewBag.Markets = markets;
+            ViewBag.Products = shoppingList.Keys.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ShoppingCart(string fullName, string address)
+        {
+            ViewBag.Message = $"Your products will be shipped at: {address}. Bon appetite, {fullName}!";
+            return View();
+        }
     }
 }
